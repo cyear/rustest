3e2c99efae039343596691e59c26dee9cf38cb92
nian0 <1274210585@qq.com>
2022-05-02 22:44:12 +0800

    cyhttp 0.1.0

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    HttpResponse::Ok().body("welcome！")
 }
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // 注释
     let ip = "0.0.0.0:8000";
     println!("start http: {} ...", ip);
     HttpServer::new(|| {
